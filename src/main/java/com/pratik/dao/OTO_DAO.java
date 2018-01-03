8a0d6366051df0ae96ae6f08761ac3a7da865c91
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2018-01-03 23:10:39 +0530

    Create OTO_DAO.java

---
+++ src/main/java/com/pratik/dao/OTO_DAO.java
@@ -0,0 +1,8 @@
+package com.pratik.dao;
+
+public interface OTO_DAO {
+    	public void SaveDataUsingStudent();
+    	public void loadDataUsingStudent();
+     	public void SaveDataUsingHallTicket();
+    	public void loadDataUsingHallTicket();
+}
